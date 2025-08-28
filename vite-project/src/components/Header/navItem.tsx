8ab7e34a30ce71f95a40729e9ba19de023055802
Giovanna <giovannamccds@gmail.com>
2025-08-28 17:20:22 -0300

    fix: font header

--- vite-project/src/components/Header/navItem.tsx
+++ vite-project/src/components/Header/navItem.tsx
@@ -9,22 +9,22 @@ export default function NavItem(props: NavLinkInterface) {
   return (
     <>
       <li>
-        <a href={props.home} className="hover:text-black font-semibold">
+        <a href={props.home} className="hover:text-black  font-medium">
           Home
         </a>
       </li>
       <li>
-        <a href={props.shop} className="hover:text-black font-semibold">
+        <a href={props.shop} className="hover:text-black font-medium">
           Shop
         </a>
       </li>
       <li>
-        <a href={props.contatUs} className="hover:text-black font-semibold">
+        <a href={props.contatUs} className="hover:text-black  font-medium">
           Contact Us
         </a>
       </li>
       <li>
-        <a href={props.blog} className="hover:text-black font-semibold">
+        <a href={props.blog} className="hover:text-black  font-medium">
           Blog
         </a>
       </li>
