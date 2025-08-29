0b221efdeb5ada97f7e912357e1d04c0a5f566f2
Eduardo Bellini <eduardombellini@gmail.com>
2025-08-29 14:11:50 -0300

    fix hero layout

--- vite-project/src/components/Hero/FullHero.tsx
+++ vite-project/src/components/Hero/FullHero.tsx
@@ -6,10 +6,11 @@ import macbookAirImage from './assets/MacBookPro14.png';
 import airpodsMaxImage from './assets/airpodMax_hero.png';
 import visionProImage from './assets/image 36.png';
 
-const FullHero = () => {
+const FullPageLayout = () => {
   return (
-    <div className="bg-white">
-      <div className="flex flex-col items-center justify-start bg-zinc-900 p-6 text-white md:min-h-[600px] md:flex-row md:justify-between md:py-0 md:pl-12 md:relative md:overflow-hidden"> 
+    <div className="bg-white relative z-0 mb-24">
+
+      <div className="flex flex-col items-center justify-start bg-zinc-900 p-6 text-white md:min-h-[600px] md:flex-row md:justify-between md:py-0 md:pl-12 md:relative md:overflow-hidden">
         <div className="flex flex-col items-center text-center md:items-start md:text-left md:pr-12 md:pb-24 md:z-10">
           <h2 className="text-sm font-light text-zinc-400">Pro.Beyond.</h2>
           <h1 className="text-5xl font-bold md:text-7xl">iPhone 14</h1>
@@ -23,7 +24,7 @@ const FullHero = () => {
             </button>
           </div>
         </div>
-        <div className="relative mt-8 md:mt-0 md:h-full md:w-full md:absolute md:right-0 md:bottom-0 md:flex md:items-end md:justify-end md:pr-10"> 
+        <div className="relative mt-8 md:mt-0 md:h-full md:w-full md:absolute md:right-0 md:bottom-0 md:flex md:items-end md:justify-end md:pr-12">
           <img
             src={iphoneImage}
             alt="iPhone 14 Pro"
@@ -31,20 +32,42 @@ const FullHero = () => {
           />
         </div>
       </div>
-      <div className="p-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:h-[600px]">
-        <div className="flex flex-col items-center justify-center p-6 bg-zinc-100 text-zinc-900 md:col-span-2 md:row-span-2">
-          <div className="relative w-full max-w-md">
-            <img src={playstation5Image} alt="Playstation 5" className="mx-auto block h-auto max-h-52 w-auto" />
-          </div>
-          <div className="mt-8 text-center">
-            <h1 className="text-4xl font-normal tracking-tight md:text-6xl">Playstation <span className="font-extrabold">5</span></h1>
-            <p className="mt-4 max-w-sm text-base text-zinc-600">
-              Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.
-            </p>
-          </div>
-        </div>
+      <div className="p-0 grid grid-cols-1 md:grid-cols-2 md:h-[600px] relative z-10">
 
-        <div className="flex flex-col items-center justify-center p-6 bg-zinc-100 text-zinc-900 md:col-span-2 md:row-span-2">
+        <div className="flex flex-col">
+            <div className="flex-1 flex flex-col items-center justify-center p-6 bg-zinc-100 text-zinc-900">
+                <div className="relative w-full max-w-md">
+                    <img src={playstation5Image} alt="Playstation 5" className="mx-auto block h-auto max-h-52 w-auto" />
+                </div>
+                <div className="mt-8 text-center">
+                    <h1 className="text-4xl font-normal tracking-tight md:text-6xl">Playstation <span className="font-extrabold">5</span></h1>
+                    <p className="mt-4 max-w-sm text-base text-zinc-600">
+                        Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.
+                    </p>
+                </div>
+            </div>
+            <div className="flex flex-col md:flex-row flex-1">
+                <div className="flex-1 flex flex-col items-center justify-center p-6 bg-zinc-100 text-zinc-900">
+                    <div className="relative w-full max-w-xs">
+                        <img src={airpodsMaxImage} alt="Apple AirPods Max" className="mx-auto block" />
+                    </div>
+                    <div className="mt-8 text-center">
+                        <h1 className="text-3xl font-light leading-tight">Apple <span className="font-bold">AirPods Max</span></h1>
+                        <p className="mt-2 text-sm text-zinc-600">Computational audio. Listen, it's powerful</p>
+                    </div>
+                </div>
+                <div className="flex-1 flex flex-col items-center justify-center p-6 bg-zinc-800 text-white">
+                    <div className="relative w-full max-w-lg">
+                        <img src={visionProImage} alt="Apple Vision Pro" className="mx-auto block" />
+                    </div>
+                    <div className="mt-8 text-center">
+                        <h1 className="text-3xl font-light leading-tight">Apple Vision <span className="font-bold">Pro</span></h1>
+                        <p className="mt-2 text-sm font-light text-zinc-400">An immersive way to experience entertainment</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="flex flex-col items-center justify-center p-6 bg-zinc-100 text-zinc-900 md:row-span-2 md:col-start-2 md:row-start-1">
           <div className="relative w-full max-w-lg">
             <img src={macbookAirImage} alt="Macbook Air" className="mx-auto block" />
           </div>
@@ -58,28 +81,9 @@ const FullHero = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center p-6 bg-zinc-100 text-zinc-900 md:col-span-1 md:row-span-1">
-          <div className="relative w-full max-w-xs">
-            <img src={airpodsMaxImage} alt="Apple AirPods Max" className="mx-auto block" />
-          </div>
-          <div className="mt-8 text-center">
-            <h1 className="text-3xl font-light leading-tight">Apple <span className="font-bold">AirPods Max</span></h1>
-            <p className="mt-2 text-sm text-zinc-600">Computational audio. Listen, it's powerful</p>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center p-6 bg-zinc-800 text-white md:col-span-1 md:row-span-1">
-          <div className="relative w-full max-w-lg">
-            <img src={visionProImage} alt="Apple Vision Pro" className="mx-auto block" />
-          </div>
-          <div className="mt-8 text-center">
-            <h1 className="text-3xl font-light leading-tight">Apple Vision <span className="font-bold">Pro</span></h1>
-            <p className="mt-2 text-sm font-light text-zinc-400">An immersive way to experience entertainment</p>
-          </div>
-        </div>
       </div>
     </div>
   );
 };
 
-export default FullHero;
+export default FullPageLayout;
