d0a12f5dce703bfc203555fa7390b319a2163c9b
Giovanna <giovannamccds@gmail.com>
2025-09-01 14:19:58 -0300

    feat: buttton component

--- vite-project/src/components/BottomBanner/bottomBanner.tsx
+++ vite-project/src/components/BottomBanner/bottomBanner.tsx
@@ -1,3 +1,5 @@
+import ButtonWhite from "../Buttons/buttonWhite"; 
+
 export default function BottomBanner() {
   return (
     <div className="w-full h-full bg-black flex justify-center">
@@ -7,9 +9,7 @@ export default function BottomBanner() {
         <h3 className="text-zinc-500 mb-8">
           Commodo fames vitae vitae leo mauris in. Eu consequat.
         </h3>
-        <button className="rounded-[6px] border border-white px-10 py-3 text-sm font-medium transition-colors text-white duration-200 hover:bg-zinc-300 ">
-          Shop Now
-        </button>
+        <ButtonWhite name="Shop Now" />
       </div>
       <div className="bg-[url('Assets/BottomBannerIMG.png')] bg-cover bg-center h-[32rem]  w-[23.4rem] justify-center text-center flex flex-col  items-center md:hidden">
         <h1 className="font-thin text-4xl text-white mb-5">Big Summer</h1>
@@ -17,9 +17,7 @@ export default function BottomBanner() {
         <h3 className="text-zinc-500 mb-8">
           Commodo fames vitae vitae leo mauris in. Eu consequat.
         </h3>
-        <button className="rounded-[6px] border border-white px-10 py-3 text-sm font-medium transition-colors text-white duration-200 hover:bg-zinc-300">
-          Shop Now
-        </button>
+        <ButtonWhite name="Shop Now" />
       </div>
     </div>
   );
---
+++ vite-project/src/components/Buttons/buttonBlack.tsx
@@ -0,0 +1,11 @@
+export interface nameButton{
+    name: string;
+}
+
+export default function buttonWhite(props: nameButton) {
+  return (
+    <button className="rounded-[6px] border border-zinc-700 px-10 py-3 text-sm font-medium transition-colors text-zinc-700 duration-200 hover:bg-zinc-300">
+        {props.name}
+    </button>
+  )
+}
