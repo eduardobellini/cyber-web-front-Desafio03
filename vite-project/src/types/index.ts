d7722cae3699862b5a3ae2b1aea7046831378cd5
Thierry-DV <thierryuchoa7@gmail.com>
2025-09-05 10:24:05 -0300

    feat: implement Toast component and add animations; refactor Product interface

--- vite-project/src/types/index.ts
+++ vite-project/src/types/index.ts
@@ -1,18 +1,3 @@
-export interface Product {
-  id: number;
-  name: string;
-  description?: string;
-  price: number;
-  image: string;
-  brand: string;
-  category: Category;
-}
-
-export interface BrandData {
-  brand: string;
-  total: number;
-}
-
 export interface Category {
   id: number;
   name: string;
@@ -21,13 +6,43 @@ export interface Category {
   url_image: string;
 }
 
-export interface CategoriesResponse {
-  data: Category[];
-  metadata: {
-    total_items: number;
-    total_pages: number;
-    current_page: number;
-  };
+export interface BrandData {
+  brand: string;
+  total: number;
+}
+
+export interface SmartphoneSpecs {
+    screen_size: string | null;
+    cpu: string | null;
+    total_cores: string | null;
+    main_camera: string | null;
+    front_camera: string | null;
+    battery: string | null;
+}
+
+export interface StorageOption {
+    id: number;
+    size: string;
+}
+
+export interface ProductColor {
+    id: number;
+    name: string;
+    hex_code: string;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  description?: string;
+  price: number;
+  discounted_price: number | null;
+  image: string;
+  brand: string;
+  category: Category;
+  smartphoneSpecs: SmartphoneSpecs | null;
+  storageOptions: StorageOption[];
+  colors: ProductColor[];
 }
 
 export interface ProductsResponse {
